bd86e92ffeed4c32131319aec12487a8a0f546c0
Juan Esteban Gomez <blogmaster50@gmail.com>
2022-10-10 14:35:31 -0500

    -Se agregan las entidades, los DTOS y las creaciones, se genera el Automapper

---
+++ Entidades/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerVehiculos.Entidades
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Procedencia { get; set; }
+    }
+}
---
+++ Entidades/Procedimiento.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerVehiculos.Entidades
+{
+    public class Procedimiento
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Descripción { get; set; }
+        [Required]
+        public int HorasInvertidas { get; set; }
+        [Required]
+        public int ValorProcedimiento { get; set; }
+    }
+}
